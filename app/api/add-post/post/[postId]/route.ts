dc2abb45e335d4242fbc1116ce4679518ec77b16
Abhishek <abhishek.choudhary@ensuesoft.com>
2024-09-02 14:43:11 +0530

    Minor fix

--- app/api/add-post/post/[postId]/route.ts
+++ app/api/add-post/post/[postId]/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export const DELETE = async (request: Request, { params }: any) => {
   try {
     const id = params.postId;
+    // log
 
     const post = await prisma.post.delete({
       where: { id },
