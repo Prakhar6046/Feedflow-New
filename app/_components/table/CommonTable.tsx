5cf0dcc7a4be93142db12bce85ea676271415387
Satveer Singh <40592098+SatveerSingh1234@users.noreply.github.com>
2024-10-28 10:09:27 +0530

    .

--- app/_components/table/CommonTable.tsx
+++ app/_components/table/CommonTable.tsx
@@ -1,29 +1,28 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import { FishSupply } from "@/app/_typeModels/fishSupply";
+import { breadcrumsAction } from "@/lib/features/breadcrum/breadcrumSlice";
+import { useAppDispatch } from "@/lib/hooks";
 import {
   Box,
   Button,
-  Divider,
   Menu,
   MenuItem,
   Stack,
   TableSortLabel,
   Typography,
 } from "@mui/material";
-import { FishSupply } from "@/app/_typeModels/fishSupply";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 import { usePathname, useRouter } from "next/navigation";
-import { breadcrumsAction } from "@/lib/features/breadcrum/breadcrumSlice";
-import { useAppDispatch } from "@/lib/hooks";
+import React, { useEffect, useState } from "react";
 
-import { useAppSelector } from "@/lib/hooks";
 import { selectRole } from "@/lib/features/user/userSlice";
+import { useAppSelector } from "@/lib/hooks";
 import { getCookie } from "cookies-next";
 interface Props {
   tableData: {
