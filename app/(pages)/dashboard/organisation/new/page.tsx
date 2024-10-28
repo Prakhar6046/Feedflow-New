38f8f5b3a4f3763399f897ec0cefc98d8258ac01
Abhsiehk <abhishek.choudhary@ensuesoft.com>
2024-10-28 10:13:08 +0530

    remove code

--- app/(pages)/dashboard/organisation/new/page.tsx
+++ app/(pages)/dashboard/organisation/new/page.tsx
@@ -2,14 +2,6 @@ import AddNewOrganisation from "@/app/_components/AddNewOrganisation";
 import BasicBreadcrumbs from "@/app/_components/Breadcrumbs";
 
 export default async function Page() {
-  // const loggedUser: any = getCookie("logged-user", { cookies });
-  // const user = JSON.parse(loggedUser);
-
-  // let organisations = await getOrganisations(
-  //   user?.data?.user?.organisationId,
-  //   user?.data?.user?.role
-  // );
-
   return (
     <>
       <BasicBreadcrumbs
