7f5b9e58331046481772b90bd2fb3fd00ef3ac57
Abhsiehk <abhishek.choudhary@ensuesoft.com>
2024-11-28 13:41:23 +0530

    Make it dynamic route

--- app/api/production/batches/route.ts
+++ app/api/production/batches/route.ts
@@ -3,6 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   try {
+    const searchParams = request.nextUrl.searchParams;
+    const role = searchParams.get("role");
+    const organisationId = searchParams.get("organisationId");
+    const userId = searchParams.get("userId");
+
     const fishSupply = await prisma.fishSupply.findMany({});
     return new NextResponse(
       JSON.stringify({
