560ccb4426cc0bc16e20bba946128e86b117e26a
Abhsiehk <abhishek.choudhary@ensuesoft.com>
2024-12-04 12:31:32 +0530

    fix bug

--- app/api/production/mange/route.ts
+++ app/api/production/mange/route.ts
@@ -26,46 +26,46 @@ export async function POST(req: NextRequest) {
           productionId: Number(data.id),
         },
       });
-
-      for (const data of body.data) {
-        if (data.id) {
-          await prisma.production.update({
-            where: { id: data.id },
-            data: {
-              fishFarmId: data.fishFarm,
-              productionUnitId: data.productionUnit,
-              fishCount: data.count,
-              batchNumberId: Number(data.batchNumber),
-              biomass: data.biomass,
-              meanLength: data.meanLength,
-              meanWeight: data.meanWeight,
-              stockingLevel: data.stockingLevel,
-              stockingDensityKG: data.stockingDensityKG,
-              stockingDensityNM: data.stockingDensityNM,
-              field: data.field ?? "",
-            },
-          });
-        } else {
-          await prisma.production.create({
-            data: {
-              fishFarmId: data.fishFarm,
-              productionUnitId: data.productionUnit,
-              fishCount: data.count,
-              batchNumberId: Number(data.batchNumber),
-              biomass: data.biomass,
-              meanLength: data.meanLength,
-              meanWeight: data.meanWeight,
-              stockingLevel: data.stockingLevel,
-              stockingDensityKG: data.stockingDensityKG,
-              stockingDensityNM: data.stockingDensityNM,
-              organisationId: body.organisationId,
-              age: "",
-              field: data.field ?? "",
-            },
-          });
-        }
+    }
+    for (const data of body.data) {
+      if (data.id) {
+        await prisma.production.update({
+          where: { id: data.id },
+          data: {
+            fishFarmId: data.fishFarm,
+            productionUnitId: data.productionUnit,
+            fishCount: data.count,
+            batchNumberId: Number(data.batchNumber),
+            biomass: data.biomass,
+            meanLength: data.meanLength,
+            meanWeight: data.meanWeight,
+            stockingLevel: data.stockingLevel,
+            stockingDensityKG: data.stockingDensityKG,
+            stockingDensityNM: data.stockingDensityNM,
+            field: data.field ?? "",
+          },
+        });
+      } else {
+        await prisma.production.create({
+          data: {
+            fishFarmId: data.fishFarm,
+            productionUnitId: data.productionUnit,
+            fishCount: data.count,
+            batchNumberId: Number(data.batchNumber),
+            biomass: data.biomass,
+            meanLength: data.meanLength,
+            meanWeight: data.meanWeight,
+            stockingLevel: data.stockingLevel,
+            stockingDensityKG: data.stockingDensityKG,
+            stockingDensityNM: data.stockingDensityNM,
+            organisationId: body.organisationId,
+            age: "",
+            field: data.field ?? "",
+          },
+        });
       }
     }
+
     return NextResponse.json({
       message: "Unit Added successfully",
       status: true,
